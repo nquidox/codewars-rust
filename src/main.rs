83ad6c3b35aeb8ea5137d002ea7fe31018a92e52
nquidox <nquidox@gmail.com>
2024-05-01 22:20:53 +0300

    Reversed Strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn reversed_strings(phrase: &str) -> String {
+    let mut res = String::new();
+    for char in phrase.chars().rev(){
+        res += &char.to_string();
+    }
+    return res
+}
+
 fn main() {
     println!("Codewars");
+    println!("{}", reversed_strings("world"))
 }

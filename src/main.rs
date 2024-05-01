fc1ec5741a21feaefdd08ca527347ba12985da9c
nquidox <nquidox@gmail.com>
2024-05-01 22:52:44 +0300

    Powers of 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+#![allow(dead_code)]
 fn reversed_strings(phrase: &str) -> String {
     return phrase.chars().rev().collect()
 }
 
+fn powers_of_two(n: u8) -> Vec<u128> {
+    let mut res = Vec::new();
+    for i in 0..n+1{
+        res.push(2_u128.pow(i as u32));
+    }
+    res
+}
+
 fn main() {
     println!("Codewars");
-    println!("{}", reversed_strings("world"))
+    println!("{:?}, [1,2,4,8,16]", powers_of_two(4));
+    // println!("{}", reversed_strings("world"))
 }
